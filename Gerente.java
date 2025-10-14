326ab4ca592a7e15e11d2ec6c64dddcc31f870c8
Jullyana Breciani <123586528+J-Breciani@users.noreply.github.com>
2025-10-14 20:50:10 -0300

    Classe Gerente segundo o modelo

--- Gerente.java
+++ Gerente.java
@@ -1,3 +1,12 @@
-public class Gerente {
+public class Gerente extends Funcionario{
+    private String senha;
     
+    public Gerente(String nome, String cpf, int dia, int mes, int ano, double salario, String senha) {
+        super(nome, cpf, dia, mes, ano, salario);
+        this.senha = senha;
+    }
+    
+    public boolean validarAcesso(String senha) {
+        return senha.equals(this.senha);
+    }
 }
