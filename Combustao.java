23e4f1ef9e33a8ba1a315faf55f210b6f2a747c5
Jullyana Breciani <123586528+J-Breciani@users.noreply.github.com>
2025-10-15 09:52:56 -0300

    Update Combustao.java

--- Combustao.java
+++ Combustao.java
@@ -1,3 +1,21 @@
-public class Combustao {
+public class Combustao extends Veiculo {
+    private double autonomiaComb, capacidadeComb;
     
+    public Combustao (String marca, String modelo, int anoFab, int mesFab, 
+            int anoMod, double valor, double autonomiaComb, 
+            double capacidadeComb){
+        super(marca,modelo,anoFab,mesFab, anoMod,valor);
+        this.autonomiaComb = autonomiaComb;
+        this.capacidadeComb = capacidadeComb;
+    }
+    
+    @Override
+    public int getAutonomia() { // Conferir também
+        return (int)Math.round(autonomiaComb);
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + " (Combustão)";
+    }
 }
