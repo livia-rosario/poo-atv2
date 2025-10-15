ce8931709b575506f767aec4bc7fddcfcebf19dd
Jullyana Breciani <123586528+J-Breciani@users.noreply.github.com>
2025-10-15 09:49:18 -0300

    Update Eletrico.java

--- Eletrico.java
+++ Eletrico.java
@@ -1,3 +1,20 @@
-public class Eletrico {
+public class Eletrico extends Veiculo {
+    private double autonomiaBat, capacidadeBat;
     
+    public Eletrico (String marca, String modelo, int anoFab, int mesFab, 
+            int anoMod, double valor, double autonomiaBat, double capacidadeBat){
+        super(marca,modelo,anoFab,mesFab, anoMod,valor);
+        this.autonomiaBat = autonomiaBat;
+        this.capacidadeBat = capacidadeBat;
+    }
+    
+    @Override
+    public int getAutonomia() { // Carros elétricos não tem autonomia de combustível (CONFERIR)
+        return (int)Math.round(autonomiaBat);
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + " (Elétrico)";
+    }
 }
