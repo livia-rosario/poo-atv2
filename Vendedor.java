0b25adebdad1622165c0b3397371b4bfb63e985b
Jullyana Breciani <123586528+J-Breciani@users.noreply.github.com>
2025-10-19 21:37:19 -0300

    Update Vendedor.java

--- Vendedor.java
+++ Vendedor.java
@@ -1,3 +1,47 @@
-public class Vendedor {
+import java.util.ArrayList;
+
+public class Vendedor extends Funcionario{
+    private double comissao;
+    private ArrayList<Venda> vendidos;
+    
+    public Vendedor(String nome, String cpf, int dia, int mes, int ano, double salario, double comissao) {
+        super(nome, cpf, dia, mes, ano, salario);
+        this.comissao = comissao;
+        this.vendidos = new ArrayList<>();
+    }
+    
+    public void addVenda(Venda v) {
+        vendidos.add(v);
+    }
+    
+    public double comissaoTotal(int mes, int ano) {
+        double total = 0;
+        for (Venda v: vendidos) {
+            if (v.getData().getMes() == mes && v.getData().getAno() == ano) {
+                total =  total + (v.valor() * comissao);
+            }
+        }
+        return total;
+    }
+    
+    public double comissaoTotal(int ano) {
+        double total = 0;
+        for (Venda v: vendidos) {
+            if (v.getData().getAno() == ano) {
+                total = total + (v.valor() * comissao);
+            }
+        }
+        return total;
+    }
+    
+    @Override
+    public double getSalario(int mes, int ano) {
+        return salario + comissaoTotal(mes, ano);
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString();
+    }
     
 }
