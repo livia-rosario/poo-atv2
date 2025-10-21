64cc168d87d797f890f67d247b1740e91d6d4440
Jullyana Breciani <123586528+J-Breciani@users.noreply.github.com>
2025-10-21 19:35:13 -0300

    Update Hibrido.java

--- Hibrido.java
+++ Hibrido.java
@@ -1,3 +1,28 @@
-public class Hibrido {
+public class Hibrido extends Veiculo {
+    private double autonomiaComb;
+    private double capacidadeComb;
+    private double autonomiaBat;
+    private double capacidadeBat;
+    
+    public Hibrido(String marca, String modelo, int anoFab, int mesFab, int anoMod,
+                   double valor, double autonomiaComb, double capacidadeComb,
+                   double autonomiaBat, double capacidadeBat) {
+        super(marca, modelo, anoFab, mesFab, anoMod, valor);
+        this.autonomiaComb = autonomiaComb;
+        this.capacidadeComb = capacidadeComb;
+        this.autonomiaBat = autonomiaBat;
+        this.capacidadeBat = capacidadeBat;
+    }
+    
+    @Override
+    public String toString() {
+    // BYD Song Pro 2025/2026 - Autonomia: 1110.0km (Híbrido)
+        return super.toString() + " (Híbrido)";
+    }
+    
+    @Override
+    public int getAutonomia() {
+        return (int) Math.round(autonomiaComb + autonomiaBat);
+    }
     
 }
