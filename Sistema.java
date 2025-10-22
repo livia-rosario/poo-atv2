b0a507628cd0b4e4dc7d9b42698bc4ac4ef8da4d
Lívia Rosário <liviarosariobsi@gmail.com>
2025-10-21 22:44:16 -0300

    Update Sistema.java

--- Sistema.java
+++ Sistema.java
@@ -137,7 +137,7 @@ public class Sistema {
         for (Vendedor v: vendedores) {
             for (Venda venda : v.getVendidos()) {
                 if (venda.getData().getAno() == ano && venda.getData().getMes() == mes) {
-                    System.out.println("Vendedor: " + v.getNome() + " (Salário neste mês: R$" + v.getSalario(mes, ano) + ")");
+                    System.out.println("Vendedor: " + v.getNome() + " (Salário neste mês: RS" + v.getSalario(mes, ano) + ")");
                     System.out.println(venda);
                     System.out.println("***************************************");
                     total = total + (venda.valor());
@@ -184,6 +184,7 @@ public class Sistema {
     // public void listarTurmas() {
     //}
 }
+
 
 
 
